260a71fc721c19fb368a1acda3eef8832dfad765
bushanka <61285033+bushanka@users.noreply.github.com>
2020-05-04 10:58:22 +0300

    App_version 0.51 (chat)

--- Autotranscript/Autotransсript/Autotransсript/apps/chat/views.py
+++ Autotranscript/Autotransсript/Autotransсript/apps/chat/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='http://127.0.0.1:8000/login/')
 def index(request):
     return render(request, 'chat/chat_page.html')
 
+@login_required(login_url='http://127.0.0.1:8000/login/')
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
